053e1a5ba06f17b3774fb042364cec98e58548d1
Johannes Eriksson <joheriks@vaadin.com>
2015-01-28 11:31:33 +0200

    Formatted file

--- addon/src/main/java/org/vaadin/risto/stepper/widgetset/client/ui/AbstractDecimalStepper.java
+++ addon/src/main/java/org/vaadin/risto/stepper/widgetset/client/ui/AbstractDecimalStepper.java
@@ -4,98 +4,98 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public abstract class AbstractDecimalStepper<T extends Number & Comparable<T>>
-		extends AbstractStepper<T, T> {
+        extends AbstractStepper<T, T> {
 
-	protected char decimalSeparator = '.';
+    protected char decimalSeparator = '.';
 
-	protected int numberOfDecimals;
+    protected int numberOfDecimals;
 
-	public AbstractDecimalStepper(int numberOfDecimals) {
-		this.numberOfDecimals = numberOfDecimals;
-	}
+    public AbstractDecimalStepper(int numberOfDecimals) {
+        this.numberOfDecimals = numberOfDecimals;
+    }
 
-	@Override
-	public boolean isValidForType(String value) {
-		try {
-			parse(value);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
-	}
+    @Override
+    public boolean isValidForType(String value) {
+        try {
+            parse(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
-	@Override
-	protected String getIncreasedValue(String startValue) throws Exception {
-		T value = parse(startValue);
-		value = add(value, getStepAmount());
-		value = round(value);
-		return toString(value);
-	}
+    @Override
+    protected String getIncreasedValue(String startValue) throws Exception {
+        T value = parse(startValue);
+        value = add(value, getStepAmount());
+        value = round(value);
+        return toString(value);
+    }
 
-	@Override
-	protected String getDecreasedValue(String startValue) throws Exception {
-		T value = parse(startValue);
-		value = subtract(value, getStepAmount());
-		value = round(value);
-		return toString(value);
-	}
+    @Override
+    protected String getDecreasedValue(String startValue) throws Exception {
+        T value = parse(startValue);
+        value = subtract(value, getStepAmount());
+        value = round(value);
+        return toString(value);
+    }
 
-	@Override
-	protected boolean isSmallerThanMax(String stringValue) {
-		T value = parse(stringValue);
-		if (getMaxValue() != null && value.compareTo(getMaxValue()) > 0) {
-			return false;
-		}
+    @Override
+    protected boolean isSmallerThanMax(String stringValue) {
+        T value = parse(stringValue);
+        if (getMaxValue() != null && value.compareTo(getMaxValue()) > 0) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	protected boolean isLargerThanMin(String stringValue) {
-		T value = parse(stringValue);
-		if (getMinValue() != null && value.compareTo(getMinValue()) < 0) {
-			return false;
-		}
+    @Override
+    protected boolean isLargerThanMin(String stringValue) {
+        T value = parse(stringValue);
+        if (getMinValue() != null && value.compareTo(getMinValue()) < 0) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public T parseStringValue(String value) {
-		if (value == null || "".equals(value))
-			return null;
-		return parse(value);
-	}
+    @Override
+    public T parseStringValue(String value) {
+        if (value == null || "".equals(value))
+            return null;
+        return parse(value);
+    }
 
-	@Override
-	public T parseStepAmount(String value) {
-		return parseStringValue(value);
-	}
+    @Override
+    public T parseStepAmount(String value) {
+        return parseStringValue(value);
+    }
 
-	public int getNumberOfDecimals() {
-		return numberOfDecimals;
-	}
+    public int getNumberOfDecimals() {
+        return numberOfDecimals;
+    }
 
-	public void setNumberOfDecimals(int numberOfDecimals) {
-		this.numberOfDecimals = numberOfDecimals;
-	}
+    public void setNumberOfDecimals(int numberOfDecimals) {
+        this.numberOfDecimals = numberOfDecimals;
+    }
 
-	public char getDecimalSeparator() {
-		return decimalSeparator;
-	}
+    public char getDecimalSeparator() {
+        return decimalSeparator;
+    }
 
-	public void setDecimalSeparator(char decimalSeparator) {
-		this.decimalSeparator = decimalSeparator;
-	}
+    public void setDecimalSeparator(char decimalSeparator) {
+        this.decimalSeparator = decimalSeparator;
+    }
 
-	protected abstract T parse(String value);
+    protected abstract T parse(String value);
 
-	protected abstract String toString(T value);
+    protected abstract String toString(T value);
 
-	protected abstract T round(T value);
+    protected abstract T round(T value);
 
-	protected abstract T add(T value, T increment);
+    protected abstract T add(T value, T increment);
 
-	protected abstract T subtract(T value, T decrement);
+    protected abstract T subtract(T value, T decrement);
 
 }
