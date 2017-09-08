23543e0d4a8f154bc02fc373f485262692b8b98c
Olli Tietäväinen <ollit@vaadin.com>
2017-09-08 10:30:19 +0300

    created new event instead of using Button's

--- addon/src/main/java/org/vaadin/risto/stepper/client/shared/ClickRpc.java
+++ addon/src/main/java/org/vaadin/risto/stepper/client/shared/ClickRpc.java
@@ -4,6 +4,13 @@ import com.vaadin.shared.MouseEventDetails;
 import com.vaadin.shared.communication.ServerRpc;
 
 @FunctionalInterface
+/**
+ * Server RPC that is called when the stepper is clicked
+ */
 public interface ClickRpc extends ServerRpc {
+    /**
+     * The stepper was clicked on the client
+     * @param mouseEventDetails details on the mouse that clicked (e.g. clicked button)
+     */
     void onClick(MouseEventDetails mouseEventDetails);
 }
